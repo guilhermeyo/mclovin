4544f9cdb8c2a0e9af28af20950cb640b4234adf
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-16 19:16:57 -0300

    fix redis

--- install/libraries.sh
+++ install/libraries.sh
@@ -2,4 +2,4 @@ sudo pacman -Syu --noconfirm \
   base-devel pkgconf autoconf bison rust cargo clang \
   openssl readline zlib libyaml ncurses libffi gdbm jemalloc \
   libvips imagemagick mupdf mupdf-tools \
-  redis-tools sqlite mariadb-libs postgresql-libs
+  redis sqlite mariadb-libs postgresql-libs
