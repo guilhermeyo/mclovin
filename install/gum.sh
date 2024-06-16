0072585940e435ba0ed2a4092f3bbf9b5faf58c4
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-16 19:31:06 -0300

    .

--- install/gum.sh
+++ install/gum.sh
@@ -2,7 +2,7 @@ GUM_VERSION="0.14.1" # Use known good version
 
 mkdir -p /tmp/gum-install
 cd /tmp/gum-install
-wget "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_linux_amd64.tar.gz"
+wget "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}.tar.gz"
 tar -xzf "gum_${GUM_VERSION}.tar.gz"
 
 sudo mv gum /usr/local/bin/
