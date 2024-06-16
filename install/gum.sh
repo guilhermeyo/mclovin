0f6fb889ab04855c81f2ca3d1fa19676164c3ce8
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-16 19:37:45 -0300

    more fixes

--- install/gum.sh
+++ install/gum.sh
@@ -3,9 +3,12 @@ GUM_VERSION="0.14.1" # Use known good version
 mkdir -p /tmp/gum-install
 cd /tmp/gum-install
 wget "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum-${GUM_VERSION}.tar.gz"
-tar -xzf "gum_${GUM_VERSION}.tar.gz"
+tar -xzf "gum-${GUM_VERSION}.tar.gz"
 
 sudo mv gum /usr/local/bin/
+
+rm -rf "gum-${GUM_VERSION}" "gum-${GUM_VERSION}.tar.gz"
+cd -
 
 if command -v gum &>/dev/null; then
   echo "Gum was installed successfully."
