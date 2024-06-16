83df43cdbafe4653ffd8ef80ea44a4557d303149
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-16 19:08:40 -0300

    fix path of shell

--- install/a-shell.sh
+++ install/a-shell.sh
@@ -1,5 +1,5 @@
-if ! grep -q "source $HOME/share/mclovin/config/bashrc" ~/.zshrc; then
-  echo "source $HOME/share/mclovin/config/bashrc" >> ~/.bashrc
+if ! grep -q "source $HOME/.local/share/mclovin/config/bashrc" ~/.zshrc; then
+  echo "source $HOME/.local/share/mclovin/config/bashrc" >>~/.bashrc
   echo "Profile has been added to ~/.bashrc"
 else
   echo "Profile is already sourced in ~/.bashrc"
