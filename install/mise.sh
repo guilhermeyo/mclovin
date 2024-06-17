9df03ed3c7c7e53a1b5a2d21148fe836540b198c
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-17 09:28:35 -0300

    use mise.sh script

--- install/mise.sh
+++ install/mise.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
+!/bin/bash
 
-set -e
-
-# Download and add the GPG key for mise
-wget -qO - https://mise.jdx.dev/gpg-key.pub | sudo gpg --dearmor -o /etc/pacman.d/gnupg/mise-archive-keyring.gpg
-
-# Ensure the pacman.conf.d directory exists
-sudo mkdir -p /etc/pacman.conf.d
-
-# Add mise repository to pacman's configuration
-echo -e "[mise]\nSigLevel = Required DatabaseOptional\nServer = https://mise.jdx.dev/pacman/\$arch" | sudo tee /etc/pacman.conf.d/mise.conf >/dev/null
-
-# Update pacman package database
-sudo pacman -Sy
+curl https://mise.run | sh
 
 # Install mise
 sudo pacman -Sy --noconfirm mise
