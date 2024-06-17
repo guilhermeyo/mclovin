8358121095b5bc6ced974206f4beb44a414803a9
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-17 10:53:10 -0300

    fix shell

--- install/a-shell.sh
+++ install/a-shell.sh
@@ -1,6 +1,6 @@
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
-cp ~/.local/share/mclovin/configs/bashrc ~/.bashrc
+cp ~/.local/share/mclovin/config/bashrc ~/.bashrc
 source $HOME/.bashrc
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
-cp ~/.local/share/omakub/configs/inputrc ~/.inputrc
+cp ~/.local/share/mclovin/config/inputrc ~/.inputrc
