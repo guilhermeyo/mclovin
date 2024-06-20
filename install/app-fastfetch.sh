9370e40d0fcf43c5fb331fc80b792abe7768d45d
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-20 17:16:42 -0300

    allow-downgradeS

--- install/app-fastfetch.sh
+++ install/app-fastfetch.sh
@@ -1,6 +1,6 @@
 FASTFETCH_VERSION="2.16.0" # Use known good version
 cd /tmp
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/${FASTFETCH_VERSION}/fastfetch-linux-amd64.deb
-sudo apt install -y ./fastfetch-linux-amd64.deb --allow-downgrade
+sudo apt install -y --allow-downgrades ./fastfetch-linux-amd64.deb
 rm fastfetch-linux-amd64.deb
 cd -
