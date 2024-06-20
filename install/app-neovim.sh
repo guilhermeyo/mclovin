cd9798454c458914ca3ef35b2835782c1b721814
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-20 18:19:47 -0300

    update

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -2,6 +2,6 @@ cd /tmp
 wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
 tar -xf nvim.tar.gz
 sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
-sudo cp -r nvim/runtime /usr/share/nvim/
+sudo cp -r nvim-linux64/runtime /usr/share/nvim/
 rm -rf nvim-linux64 nvim.tar.gz
 cd -
