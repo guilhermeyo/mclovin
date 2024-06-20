7fc8ae06986f69d578393d9579e9eb8ce64582e5
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-06-20 17:04:49 -0300

    fix fastfetch

--- install/app-fastfetch.sh
+++ install/app-fastfetch.sh
@@ -1,7 +1,3 @@
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
-sudo apt update -y
-sudo apt install -y fastfetch
-
 FASTFETCH_VERSION="2.16.0" # Use known good version
 cd /tmp
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/${FASTFETCH_VERSION}/fastfetch-linux-amd64.deb
