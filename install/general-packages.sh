738b5a03b99b1a96c54205e0fa2e774eb1933fba
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2024-07-07 17:31:36 -0300

    update suspend

--- install/general-packages.sh
+++ install/general-packages.sh
@@ -1,12 +1,14 @@
 sudo apt install -y \
-	vlc \
-	fzf \
-	ripgrep \
-	bat \
-	zoxide \
-	bpytop \
-	fd-find \
-	xournalpp \
-	lm-sensors \
-	keychain \
-	flameshot
+  vlc \
+  fzf \
+  ripgrep \
+  bat \
+  zoxide \
+  bpytop \
+  fd-find \
+  xournalpp \
+  lm-sensors \
+  keychain \
+  flameshot \
+  kdeconnect \
+  pm-utils
