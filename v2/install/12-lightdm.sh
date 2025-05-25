055c97e14b7476f3d290c214f87251aa8c80d6b8
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2025-05-25 16:37:02 -0300

    new fix

--- v2/install/12-lightdm.sh
+++ v2/install/12-lightdm.sh
@@ -1,7 +1,4 @@
-# Enable LightDM service
-
-echo "LightDM Litarvan - Enabling service..."
-sudo systemctl enable lightdm#!/bin/bash
+#!/bin/bash
 
 echo "LightDM Litarvan - Starting installation..."
 
@@ -9,13 +6,12 @@ echo "LightDM Litarvan - Starting installation..."
 sudo pacman -S --needed lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan --noconfirm
 
 # Configure LightDM greeter
-#
 echo "LightDM Litarvan - Configuring greeter..."
 sudo sed -i 's/^#*greeter-session=.*/greeter-session=lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
 
 # Configure Litarvan theme
 echo "LightDM Litarvan - Setting theme..."
-sudo sed -i 's/^#*webkit_theme=.*/webkit_theme=litarvan/' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo sed -i 's/^#*webkit_theme\s*=\s*antergos/webkit_theme=litarvan/' /etc/lightdm/lightdm-webkit2-greeter.conf
 
 # Enable LightDM service
 echo "LightDM Litarvan - Installation completed!"
