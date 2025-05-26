aa3d5c2be62b218a0f2f90ce792f4a051181858c
Guilherme Yamakawa de Oliveira <github@guilherme44.com>
2025-05-25 22:25:54 -0300

    fix install dev tools verification

--- v2/install/02-dev-tools.sh
+++ v2/install/02-dev-tools.sh
@@ -2,7 +2,6 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_DIR="$(dirname "$SCRIPT_DIR")"
-
 source "$BASE_DIR/utils.sh"
 
 packages=(
@@ -26,7 +25,20 @@ echo "Development tools - Starting installation..."
 install_packages "${packages[@]}"
 
 # Install tmux powers
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+if [ -d ~/.tmux/plugins/tpm ]; then
+  echo "TPM is already installed - skipping..."
+else
+  echo "Installing TPM..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  echo "TPM installation completed!"
+fi
+
+if [ -d ~/.tmuxifier ]; then
+  echo "Tmuxifier is already installed - skipping..."
+else
+  echo "Installing Tmuxifier..."
+  git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+  echo "Tmuxifier installation completed!"
+fi
 
 echo "Development tools - Installation completed!"
